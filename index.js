0367a5e006110fe1adfec4af474f0df19b345a07
lguerrero1 <luisguerrero12387@gmail.com>
2021-06-23 23:23:58 -0400

    connecting to db

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 import  express  from 'express';
 import router from './routes/index.js';
+import db from './config/db.js'
 
 const app = express()
+//conectar a base de datos 
+db.authenticate()
+  .then( () => console.log('base de datos conectada'))
+  .catch(error => console.log(error));
+
 //definir puerto
 const port = process.env.PORT || 4000;
 
 //habilitar PUG
 app.set('view engine', 'pug');
 
+//obtener el a;o actual
+app.use((req,res,next)=> {
+  const year = new Date();
+
+  res.locals.actualYear = year.getFullYear();
+  res.locals.nombreSitio = 'Agencia de viajes';
+  return next();
+})
+
 //agregar router
 app.use('/', router);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+ 
+});
 //definir la carpeta publica
 app.use(express.static("public"));
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,30 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+    res.render('inicio', {
+        pagina: 'Inicio'
+      });
+});
 
 router.get('/nosotros', (req, res) => {
-    const viajes = 'viajes a Alemania'
     res.render('nosotros',{
-        viajes
+        pagina : 'Nosotros'
     });
 
-})
+});
+router.get('/viajes', (req, res) => {
+    res.render('viajes',{
+        pagina : 'Viajes'
+    });
 
-router.get('/inicio', (req, res) => {
-    res.render('inicio',{
-        
-    })
-})
+});
 
-router.get('/contacto', (req, res) => {
-    res.send('Hello World!')
-})
+router.get('/testimoniales', (req, res) => {
+    res.render('testimoniales',{
+        pagina : 'Testimoniales'
+    });
+
+});
+
 
 export default router;
